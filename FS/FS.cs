825c32d25722fdd435dd065c563a258bffe720b0
Skiiru <metalfrost94@mail.ru>
2014-05-02 16:22:57 +0400

    IEHistory

---
+++ FS/FS.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS
+{
+    public class FS
+    {
+        List<FileSystemWatcher> fs_lst;
+        public FS()
+        {
+            try
+            {
+                foreach (string x in Environment.GetLogicalDrives())
+                {
+                    if (x == "C:\\")
+                    {
+                        FileSystemWatcher fs = new FileSystemWatcher(x);
+                        fs.Filter = "Documents and settings";
+                        fs_lst.Add(fs);
+                    }
+                    else
+                        fs_lst.Add(new FileSystemWatcher(x));
+                }
+            }
+            catch { };
+            foreach (FileSystemWatcher x in fs_lst)
+            {
+                x.IncludeSubdirectories = true;
+                x.EnableRaisingEvents = true;
+                x.Changed += new FileSystemEventHandler(x_Changed);
+                x.Created += new FileSystemEventHandler(x_Changed);
+                x.Deleted += new FileSystemEventHandler(x_Changed);
+                x.Renamed += new RenamedEventHandler(x_Renamed);
+
+            }
+        }
+        
+        void x_Renamed(object sender, RenamedEventArgs e)
+        {
+            
+        }
+        void x_Changed(object sender, FileSystemEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GKH/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("GKH")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("GKH")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c6b64767-ba58-4d4b-8cad-783a2915fffe")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
