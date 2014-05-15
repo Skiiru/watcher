8bda992b0546dbbddba803fa47e7f545de00aa82
Skiiru <metalfrost94@mail.ru>
2014-05-15 23:19:14 +0400

    все браузеры

--- History/History.cs
+++ History/History.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using UrlHistoryLibrary;
 using System.IO;
+using System.Data.SQLite;
 using System.Data;
+
 
 namespace History
 {
@@ -182,4 +184,48 @@ namespace History
             return DT;
         }
     }
+
+    public class HistoryItem
+    {
+        public string URL { get; set; }
+        public string Title { get; set; }
+        public DateTime VisitedTime { get; set; }
+    }
+    public class GoogleChrome
+    {
+        static void Main()
+	{
+		string chromeHistoryFile = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)+ @"\Google\Chrome\User Data\Default\History";
+		if (File.Exists(chromeHistoryFile))
+		{
+			SQLiteConnection connection = new SQLiteConnection("Data Source=" + chromeHistoryFile + ";Version=3;New=False;Compress=True;");
+			connection.Open();
+			DataSet dataset = new DataSet();
+			SQLiteDataAdapter adapter = new SQLiteDataAdapter("select * from urls order by last_visit_time desc", connection);
+			adapter.Fill(dataset);
+			if (dataset != null && dataset.Tables.Count > 0 & dataset.Tables[0] != null)
+			{
+				DataTable dt = dataset.Tables[0];
+                List<HistoryItem> allHistoryItems = new List<HistoryItem>();
+				foreach (DataRow historyRow in dt.Rows)
+				{
+					HistoryItem historyItem = new HistoryItem();
+					{
+						string url = Convert.ToString(historyRow["url"]);
+                        string Title = Convert.ToString(historyRow["title"]);
+					};
+					// Chrome stores time elapsed since Jan 1, 1601 (UTC format) in microseconds
+					long utcMicroSeconds = Convert.ToInt64(historyRow["last_visit_time"]);
+					// Windows file time UTC is in nanoseconds, so multiplying by 10
+					DateTime gmtTime = DateTime.FromFileTimeUtc(10 * utcMicroSeconds);
+					// Converting to local time
+					DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(gmtTime, TimeZoneInfo.Local);
+					historyItem.VisitedTime = localTime;
+					allHistoryItems.Add(historyItem);
+				}
+			}
+		}
+	}
+    }
+
 }
