9cbee9705622deeda2d89936e3b56700eb998a07
Skiiru <metalfrost94@mail.ru>
2014-05-19 13:03:43 +0400

    недостающие файлы

---
+++ Proc/Proc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security.Permissions;
+
+namespace Proc
+{
+    public class Proc
+    {
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void timer_tik()
+        {
+            foreach (Process x in System.Diagnostics.Process.GetProcesses())
+            {
+                if (x.ProcessName != "SearchFilterHost")
+                {
+                    try
+                    {
+                        x.EnableRaisingEvents = true;
+                        x.Exited += new EventHandler(x_Exited);
+                    }
+                    catch { };
+                }
+            }
+        }
+
+        void x_Exited(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
